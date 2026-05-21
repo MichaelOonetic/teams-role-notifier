9180a16513538e2ca0947ed39dcb5efeacb9048e
MichaelOonetic <michael.corre@oonetic.com>
2026-05-21 09:46:10 +0000

    initial webhook for monday

---
+++ teams-role-notifier-app/app/api/webhook/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    success: true,
+    message: "Webhook GET OK",
+  });
+}
+
+export async function POST(req: any) {
+  const body = await req.json();
+
+  console.log("Webhook reçu :", body);
+
+  return NextResponse.json({
+    success: true,
+    message: "Webhook reçu avec succès",
+  });
+}
