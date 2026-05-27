b5b18f22af1d8b37595b235124f9c9be6d2b4ef7
MichaelOonetic <michael.corre@oonetic.com>
2026-05-27 11:42:07 +0000

    add microsoft graph auth

--- teams-role-notifier-app/lib/teams.ts
+++ teams-role-notifier-app/lib/teams.ts
@@ -1,18 +1,41 @@
+async function getAccessToken() {
+
+  const response = await fetch(
+    `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/token`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type":
+          "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        client_id:
+          process.env.AZURE_CLIENT_ID!,
+        client_secret:
+          process.env.AZURE_CLIENT_SECRET!,
+        scope:
+          "https://graph.microsoft.com/.default",
+        grant_type:
+          "client_credentials"
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  console.log("ACCESS TOKEN:");
+  console.log(data);
+
+  return data.access_token;
+}
+
 export async function sendTeamsMessage(
   text: string
 ) {
 
-  await fetch(
-    process.env.TEAMS_WEBHOOK_URL!,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        text
-      })
-    }
-  );
+  const token = await getAccessToken();
+
+  console.log("TOKEN OK");
+  console.log(token);
 
 }
